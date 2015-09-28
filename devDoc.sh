a461e1ac96ca8ac6f135c416b2c834f2cd17d310
David Braun <David.Braun@Toptal.com>
2015-09-28 18:39:11 -0400

    Generate documentation using the npm script.

--- devDoc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Until gulp is added.
-
-rm -rf ./doc
-jsdoc -t ./node_modules/ink-docstrap/template -c ./jsdoc_cfg/jsdoc.conf.json ./index.js ./lib/*.js ./lib/rpc/*.js README.md -d ./doc/ --access all
