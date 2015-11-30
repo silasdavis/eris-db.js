dfff85c33bcec985de5fffbbd220e9b121fca520
David Braun <David.Braun@Toptal.com>
2015-11-30 00:48:08 -0500

    Modify publishDoc.sh for this repository.

--- publishDoc.sh
+++ publishDoc.sh
@@ -3,47 +3,41 @@
 # -------------------------------------------------------------------
 # Set vars (change if used in another repo)
 
-base_name=eris-cli
+base_name=eris-db.js
 user_name=eris-ltd
 docs_site=docs.erisindustries.com
 
 # -------------------------------------------------------------------
 # Set vars (usually shouldn't be changed)
 
-if [ "$CIRCLE_BRANCH" ]
-then
-  repo=`pwd`
-else
-  repo=$GOPATH/src/github.com/$user_name/$base_name
-fi
-release_min=$(cat $repo/version/version.go | tail -n 1 | cut -d \  -f 4 | tr -d '"')
+repo=`pwd`
+release_min=$(cat package.json | jq --raw-output .version)
 start=`pwd`
 
 # -------------------------------------------------------------------
 # Build
 
-mkdir -p docs/$base_name
-go run docs/generator.go
-
-if [[ "$1" == "master" ]]
-then
-  mkdir -p docs/$base_name/latest
-  rsync -av docs/$base_name/$release_min/ docs/$base_name/latest/
-fi
+npm run doc
 
 cd $HOME
 git clone git@github.com:$user_name/$docs_site.git
-cd $repo
+cd $docs_site/documentation
+mkdir --parents $base_name
+cd $base_name
+mv $repo/doc $release_min
 
-rsync -av docs/$base_name $HOME/$docs_site/documentation/
+if [[ "$1" == "master" ]]; then
+  ln --symbolic $release_min latest
+fi
 
 # ------------------------------------------------------------------
 # Commit and push if there's changes
 
-cd $HOME/$docs_site
 if [ -z "$(git status --porcelain)" ]; then
   echo "All Good!"
 else
+  git config --global user.email "billings@erisindustries.com"
+  git config --global user.name "Billings the Bot"
   git add -A :/ &&
   git commit -m "$base_name build number $CIRCLE_BUILD_NUM doc generation" &&
   git push origin master
