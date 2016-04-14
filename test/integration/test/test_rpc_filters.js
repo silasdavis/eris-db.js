1828a9085030ef88c4ec4c9ca01dd57f0d65cbed
David Braun <David.Braun@Toptal.com>
2016-04-13 23:30:15 -0400

    Fix integration test test_rpc_filters.

--- test/integration/test/test_rpc_filters.js
+++ test/integration/test/test_rpc_filters.js
@@ -4,7 +4,8 @@
 'use strict';
 
 var
-  createDb = require('../createDb');
+  createDb = require('../createDb'),
+  Promise = require('bluebird');
 
 var util = require('../../../lib/util');
 var asrt;
@@ -27,12 +28,22 @@ describe('ErisDbHttpFilters', function () {
     describe('#getAccounts', function () {
         this.timeout(30 * 1000);
 
-        before(function (done) {
-          createDb().spread(function (hostname, port) {
+        before(function () {
+          return createDb().spread(function (hostname, port, validator) {
+            var
+              txs;
+
             edb = edbModule.createInstance("http://" + hostname + ":" + port
               + "/rpc");
-              
-            done();
+
+            txs = edb.txs();
+            Promise.promisifyAll(txs);
+
+            return Promise.all(testData.chain_data.genesis.accounts.map(
+              function (account) {
+                return txs.sendAndHoldAsync(validator.priv_key[1],
+                  account.address, account.amount, null);
+              }));
           });
         });
 
@@ -45,7 +56,10 @@ describe('ErisDbHttpFilters', function () {
         it("should get the filtered list of accounts", function (done) {
             var filters = testData.GetAccounts1.input;
             var exp = testData.GetAccounts1.output;
-            edb.accounts().getAccounts(filters, check(exp, done));
+
+            edb.accounts().getAccounts(filters, function (error, data) {
+              check(exp, done)(error, {accounts: data.accounts.slice(0, -1)});
+            });
         });
 
         it("should get the filtered list of accounts", function (done) {
