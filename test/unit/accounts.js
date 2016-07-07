65160ed07f4ea3773a9400278b6cc8badf5ba2b4
David Braun <David@ErisIndustries.com>
2016-07-07 13:43:07 -0400

    Send permission changing transactions via Eris DB's port.
    Previously we were sending them through Tendermint's port.

--- test/unit/accounts.js
+++ test/unit/accounts.js
@@ -5,27 +5,17 @@ var assert = require('assert')
 var Accounts = rewire('../../lib/accounts')
 var MockClient = require('../mock/mock_client')
 var template = require('../mock/test_template')
-var td = require('testdouble')
 var testData = require('../testdata/testdata_mock.json')
 
 var handlers = template.getHandlers(testData)
 
-function mockJaysonRequest (request) {
-  Accounts.__set__('jayson', {
-    client: {
-      http: function () {
-        return {request: request}
-      }
-    }
-  })
-}
+describe('accounts', () => {
+  function mockAccount (options) {
+    var mockErisDb
+    var accounts
 
-describe('accounts', function () {
-  var mockErisDb
-  var accounts
-  var account
+    options = options || {}
 
-  before(function (done) {
     mockErisDb = {
       accounts: function () { return accounts },
 
@@ -34,97 +24,88 @@ describe('accounts', function () {
         sign: function () { return Promise.resolve(Buffer('')) }
       },
 
-      user: testData.GetAccount.input.address
+      user: options.noUser ? undefined : testData.GetAccount.input.address,
+
+      txs: () => ({
+        broadcastTx: (transaction, callback) => {
+          assert(options.broadcastTxTest(transaction))
+          callback()
+        }
+      })
     }
 
     accounts = Accounts.createInstance(new MockClient(handlers), null,
       mockErisDb)
 
-    accounts.getAccount(testData.GetAccount.input.address,
-      function (error, testAccount) {
-        assert.ifError(error)
+    return new Promise((resolve, reject) => {
+      accounts.getAccount(testData.GetAccount.input.address,
+        function (error, testAccount) {
+          if (error) {
+            reject(error)
+          }
 
-        account = testAccount
-        done()
-      })
-  })
+          if (options.getAccount) {
+            accounts.getAccount = options.getAccount
+          }
 
-  beforeEach(function () {
-    mockErisDb.user = '37236DF251AB70022B1DA351F08A20FB52443E37'
-  })
-
-  afterEach(function () {
-    td.reset()
-  })
+          resolve(testAccount)
+        })
+    })
+  }
 
   describe('getAccount', function () {
     describe('setPermission', function () {
-      it('warns about attempting to set unsupported permissions',
-        function () {
-          return account.setPermission('send', false).catch(function (reason) {
+      it('warns about attempting to set unsupported permissions', () =>
+        mockAccount().then(account =>
+          account.setPermission('send', false).catch(reason => {
             assert.equal(reason.message, 'Unsupported permission type: send')
           })
-        })
+        )
+      )
 
-      it('warns if the user is not specified', function () {
-        delete mockErisDb.user
+      it('warns if the user is not specified', (done) => {
+        mockAccount({noUser: true})
+          .then(account => account.setPermission('call', false))
+          .catch(reason => {
+            assert.equal(
+              reason.message,
+              "Can't set permissions unless logged in."
+            )
 
-        return account.setPermission('call', false).catch(function (reason) {
-          assert.equal(reason.message,
-            "Can't set permissions unless logged in.")
-        })
-      })
-
-      it('the first transaction in the sequence includes the public key',
-        function (done) {
-          mockJaysonRequest(function (method, params) {
-            assert('pub_key' in params[0][1].input)
             done()
           })
-
-          return account.setPermission('call', false)
-        })
-
-      it('later transactions in the sequence omit the public key',
-        function (done) {
-          mockJaysonRequest(function (method, params) {
-            assert(!('pub_key' in params[0][1].input))
-            done()
-          })
-
-          td.replace(accounts, 'getAccount', function (address, callback) {
-            callback(null, {sequence: 10})
-          })
-
-          return account.setPermission('call', false)
-        })
-
-      it('sets the call permission to false', function (done) {
-        mockJaysonRequest(function (method, params) {
-          assert(!params[0][1].args[1].value)
-          done()
-        })
-
-        return account.setPermission('call', false)
       })
 
-      it('sets the call permission to true', function (done) {
-        mockJaysonRequest(function (method, params) {
-          assert(params[0][1].args[1].value)
-          done()
-        })
+      it('the first transaction in the sequence includes the public key', () =>
+        mockAccount({
+          broadcastTxTest: transaction => 'pub_key' in transaction[1].input
+        }).then(account => account.setPermission('call', false))
+      )
 
-        return account.setPermission('call', true)
-      })
+      it('later transactions in the sequence omit the public key', () =>
+        mockAccount({
+          broadcastTxTest: transaction => !('pub_key' in transaction[1].input),
+          getAccount: (address, callback) => { callback(null, {sequence: 10}) }
+        }).then(account => account.setPermission('call', false))
+      )
 
-      it('creates a signature', function (done) {
-        mockJaysonRequest(function (method, params) {
-          assert('signature' in params[0][1].input)
-          done()
-        })
+      it('sets the call permission to false', () =>
+        mockAccount({
+          broadcastTxTest: transaction => !transaction[1].args[1].value
+        }).then(account => account.setPermission('call', false))
+      )
 
-        return account.setPermission('call', false)
-      })
+      it('sets the call permission to true', () =>
+        mockAccount({
+          broadcastTxTest: transaction => transaction[1].args[1].value
+        }).then(account => account.setPermission('call', true))
+      )
+
+      it('creates a signature', () =>
+        mockAccount({
+          broadcastTxTest: transaction => 'signature' in transaction[1].input
+        }).then(account => account.setPermission('call', false))
+      )
     })
   })
 })
