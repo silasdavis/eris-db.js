70faea62a0722fc8faac2eaff7fc73f34d6d2204
David Braun <David@ErisIndustries.com>
2016-09-04 19:07:10 -0400

    Delete all tests.
    We were testing the wrong things with brittle tests.

--- test/integration/test/testevent.sol
+++
@@ -1,7 +0,0 @@
-contract testevent {
-    event TestEvent(int indexed anIndexedInt, bytes32 indexed anIndexedBytes32, bool anUnIndexedBool);
-
-    function(){
-        TestEvent(-1, "haha", true);
-    }
-}
